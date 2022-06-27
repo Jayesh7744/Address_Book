85bd619d3d6c969198405de515d91314511fe3f8
Jayesh7744 <Jayeshs7744@gmail.com>
2022-06-27 21:58:25 +0530

    Added: Add Your Info

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,53 @@
 package com.Day_9;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook System");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addDetails();
+
+    }
+
+    public void addDetails(){
+        String firstName;
+        String lastName;
+        String address;
+        String city;
+        String state;
+        int ZipCode;
+        int mobileNumber;
+        String emailId;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name ");
+        firstName = sc.nextLine();
+
+        System.out.println("Enter Last Name ");
+        lastName = sc.nextLine();
+
+        System.out.println("Enter Address ");
+        address = sc.nextLine();
+
+        System.out.println("Enter City Name ");
+        city = sc.nextLine();
+
+        System.out.println("Enter State Name ");
+        state = sc.nextLine();
+
+        System.out.println("Enter Zipcode ");
+        ZipCode = sc.nextInt();
+
+        System.out.println("Enter Mobile no ");
+        sc.next();
+        mobileNumber = sc.nextInt();
+
+        System.out.println("Enter EmailId ");
+        emailId = sc.nextLine();
+        
+
     }
 }
+
